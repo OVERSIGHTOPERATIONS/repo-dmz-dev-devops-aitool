8d7e89e013fa36b025af049cc32b25aee0758612
David Dellanno <david.dellanno@oversightoperations.com>
2025-02-04 19:50:27 -0500

    update ai code to app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 import os
+# Azure OpenAI
+from langchain_openai import AzureChatOpenAI
+# OpenAI
+from langchain_openai import ChatOpenAI
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
 app = Flask(__name__)
 
+# Azure OpenAI
+azure_endpoint = os.environ['AZURE_OPENAI_ENDPOINT']
+azure_deployment = os.environ['AZURE_OPENAI_DEPLOYMENT_NAME']
+model_name = os.environ['OPENAI_MODEL_NAME']
+api_version = os.environ['AZURE_OPENAI_API_VERSION']
+# Only needed if you're not using managed identity
+api_key = os.environ['OPENAI_API_KEY']
+
+# OpenAI
+# api_key = os.environ['OPENAI_API_KEY']
 
 @app.route('/')
 def index():
@@ -18,15 +32,28 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+    req = request.form.get('req')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    # Azure OpenAI
+    llm = AzureChatOpenAI(
+        azure_endpoint=azure_endpoint,
+        azure_deployment=azure_deployment,
+        model_name=model_name,
+        api_version=api_version,
+        api_key=api_key
+    )
+    text = llm.invoke(req).content
 
+    # OpenAI
+    #llm = ChatOpenAI(openai_api_key=api_key)
+    #text = llm.invoke(req).content
+
+    if req:
+        print('Request for hello page received with req=%s' % req)
+        return render_template('hello.html', req=text)
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
